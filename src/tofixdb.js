47f468af6054b6d4ec12e9853b4ad2d4d3262496
Rub21 <ruben@mapbox.com>
2016-03-11 13:47:14 -0500

    clean up

--- src/tofixdb.js
+++ src/tofixdb.js
@@ -1,9 +1,7 @@
 'use strict';
-
 var queue = require('queue-async');
 var _ = require('underscore');
 var pg = require('pg');
-
 var user = process.env.DBUsername || 'postgres';
 var password = process.env.DBPassword || '';
 var address = process.env.DBAddress || 'localhost';
@@ -19,11 +17,10 @@ module.exports = {
 	getItems: function(idtask, done) {
 		pg.connect(conString, function(err, client, d) {
 			if (err) return console.log(err);
-			// console.log('connected to:', address);
 			var task;
 			queue(1)
 				.defer(function(cb) {
-					var query = 'SELECT id, title, source, tasktable, description, updated, status  FROM task_details where id = $1;';
+					var query = 'SELECT id, tasktable FROM task_details where id = $1;';
 					var cliente = client.query(query, [idtask], function(err, results) {
 						task = results.rows[0];
 						cb();
@@ -38,7 +35,6 @@ module.exports = {
 						var header;
 						var body;
 						var rows = results.rows;
-						//replace(/\|/gi, '').
 						for (var i = 0; i < rows.length; i++) {
 							var obj = JSON.parse(rows[i].value.split('|').join('"'));
 							if (i == 0) items.push(_.keys(obj).join(','));
