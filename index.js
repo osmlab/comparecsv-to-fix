60356e5bad05ba3748ec693c0218bcfff561acac
Rub21 <ruben@mapbox.com>
2016-10-13 12:09:39 +0530

    compare geojson

--- index.js
+++ index.js
@@ -2,22 +2,36 @@
 
 'use strict';
 var fs = require('fs');
-var pg = require('pg');
-
+var request = require('request');
 var readline = require('readline');
-var tofixdb = require('./src/tofixdb');
 var argv = require('minimist')(process.argv.slice(2));
-var idtask = argv.idtask;
+var urlTask = argv.urlTask;
 var comparefile = argv._[0];
-tofixdb.getItems(idtask, function(items) {
-  var rd = readline.createInterface({
-    input: fs.createReadStream(comparefile),
-    output: process.stdout,
-    terminal: false
-  });
-  rd.on('line', function(line) {
-    if (items.indexOf(line) === -1) {
-      console.log(line);
+request(urlTask, function(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var items = JSON.parse(body);
+    var geometryofItems = [];
+    for (var i = 0; i < items.length; i++) {
+      geometryofItems.push(JSON.stringify(items[i].geometry));
     }
-  });
+    var rd = readline.createInterface({
+      input: fs.createReadStream(comparefile),
+      output: process.stdout,
+      terminal: false
+    });
+    rd.on('line', function(line) {
+      var geojson = {
+        type: 'FeatureCollection',
+        features: []
+      };
+      var newitems = JSON.parse(line);
+      for (var i = 0; i < newitems.features.length; i++) {
+        var geoString = newitems.features[i].geometry;
+        if (items.indexOf(geoString) === -1) {
+          geojson.features.push(newitems.features[i]);
+        }
+      }
+      process.stdout.write(JSON.stringify(geojson) + '\n');
+    });
+  }
 });
